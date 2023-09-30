f2b1b55d69638a7f5cafba72f727d993dbac2937
JoeTerol <teroljoseeduardo@gmail.com>
2023-09-30 19:49:34 -0300

    feat: crud done in categories service

--- Services/categories.service.js
+++ Services/categories.service.js
@@ -16,19 +16,40 @@ class CategoresServices {
       });
     }
   }
-  create() {
-
+  create(data) {
+    const newCategory = {
+      id: faker.datatype.uuid(),
+      ...data
+    }
+    this.categories.push(newCategory);
+    return newCategory
   }
   find() {
+    return this.categories;
 
   }
   findOne(id) {
     return this.categories.find(item => item.id === id);
   }
-  update() {
+  update(id, changes) {
+    const index = this.categories.findIndex(item => item.id === id);
+    if (index === -1) {
+      throw new Error('category not found');
+    }
+    const category = this.categories[index];
+    this.categories[index] = {
+      ...category,
+      ...changes
+    }
 
   }
-  delete() {
+  delete(id) {
+    const index = this.categories.findIndex(item => item.id === id);
+    if (index === -1) {
+      throw new Error('category not found');
+    }
+    this.categories.splice(index, 1);
+    return { id };
 
   }
 }
