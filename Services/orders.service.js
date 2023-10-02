829832adcdbb57de8116646f019a71ed5369b76c
JoeTerol <teroljoseeduardo@gmail.com>
2023-10-02 17:11:32 -0300

    feat: crud done in orders.service

--- Services/orders.service.js
+++ Services/orders.service.js
@@ -17,22 +17,43 @@ class OrdersService {
       });
     }
   }
-  create() {
+  create(data) {
+    const newOrder = {
+      id: faker.datatype.uuid(),
+      ...data
+    }
+    this.orders.push(newOrder);
+    return newOrder;
 
   }
   find() {
-    return this.products;
+    return this.Orders;
 
   }
   findOne(id) {
     return this.orders.find(item => item.id === id);
 
   }
-  update() {
+  update(id, changes) {
+  const index = this.orders.findIndex(item => item.id === id)
+  if (index === -1) {
+    throw new Error('order not found');
 
   }
-  delete() {
+  const order = this.orders[index];
+  this.orders[index] = {
+    ...order,
+    ...changes
+  };
 
+  }
+  delete(id) {
+    const index = this.orders.findIndex(item => item.id === id);
+    if (index === -1) {
+      throw new Error('order not found')
+    }
+    this.orders.splice(index, 1);
+    return { id };
   }
 }
 module.exports = OrdersService;
